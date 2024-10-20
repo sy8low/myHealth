3ca7264f256f1eb9726db0ef693f3cb65b451020
sy8low <sy8low@gmail.com>
2024-10-20 10:21:01 +0800

    Acknowledged source of solution.

--- src/myHealth/__init__.py
+++ src/myHealth/__init__.py
@@ -15,4 +15,4 @@ Note:
 # 1. Create a virtual environment in myHealth_package: python3 -m venv .venv
 # 2. Activate it: . .venv/bin/activate
 # 3. Download all dependencies listed in requirements.txt.
-# 2. Ensure tkinter is installed, though it should already be: sudo apt-get install python3-tk
+# 4. Ensure tkinter is installed, though it should already be: sudo apt-get install python3-tk (solution provided by ChatGPT).
